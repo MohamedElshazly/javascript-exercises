1e2c8cdc203aec27f1a9b3f164691974d3735ba7
omni <mohamedelshazly18@yahoo.com>
2022-04-29 22:17:56 +0200

    all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(targetString, repeatNumber) {
+    // loop num times and concatenate the strig with itself.
+    const originalString = targetString;
+    //return empty string if repeat num is 0
+    if(repeatNumber === 0) {
+        return '';
+    }
+    else if(repeatNumber < 0) {
+        return 'ERROR';
+    }  
+    for(let i = 0; i < repeatNumber - 1; i++) {
+        targetString = targetString.concat(originalString);
+    }
+    return targetString;
 };
 
 // Do not edit below this line
